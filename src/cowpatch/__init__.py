35afbd108776a5436ec5c6d500643aaf591bec34
benjaminleroy <benjaminleroyusa@gmail.com>
2022-02-18 08:49:01 -0800

    cleanup relative to sizing error complete

--- src/cowpatch/__init__.py
+++ src/cowpatch/__init__.py
@@ -3,3 +3,6 @@ from importlib.metadata import version
 __version__ = version(__name__)
 
 from .layout import layout, area
+from .base import patch
+from .text import text
+from .wrappers import wrapper_plotnine, wrapper_matplotlib, wrapper_seaborn
